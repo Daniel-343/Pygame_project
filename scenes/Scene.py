83ce8ab3a0f68d3c04883e5e2af98ada705bcb26
Daniel-Toth <danthe34@gmail.com>
2024-02-05 17:14:48 +0100

    refactor variable names

--- scenes/Scene.py
+++ scenes/Scene.py
@@ -7,13 +7,61 @@ from objects.Structure import Structure
 from sprites.Sprite import Sprite
 
 
+def check_controls(player, zoom_scale, objects, player_speed):
+    key = pygame.key.get_pressed()
+    if (key[pygame.K_LEFT] or key[pygame.K_a]) and not player.check_if_collides(objects, -player_speed, 0):
+        player.move_left(zoom_scale)
+    if (key[pygame.K_RIGHT] or key[pygame.K_d]) and not player.check_if_collides(objects, player_speed, 0):
+        player.move_right(zoom_scale)
+    if (key[pygame.K_DOWN] or key[pygame.K_s]) and not player.check_if_collides(objects, 0, player_speed):
+        player.move_down(zoom_scale)
+    if (key[pygame.K_UP] or key[pygame.K_w]) and not player.check_if_collides(objects, 0, -player_speed):
+        player.move_up(zoom_scale)
+    if not any((key[pygame.K_LEFT], key[pygame.K_RIGHT], key[pygame.K_DOWN], key[pygame.K_UP], key[pygame.K_w],
+                key[pygame.K_a], key[pygame.K_s], key[pygame.K_d])):
+        player.state = "idle"
+
+
+def get_map_layout():
+    with open('./resources/map1.json', 'r') as file:
+        return json.load(file)["map_layout"]
+
+
+def get_collidables_below(collidables, player):
+    collidables_below = []
+    for collidable in collidables:
+        if collidable.pos.y + collidable.collision_area_y >= player.pos.y + player.collision_area_y:
+            collidables_below.append(collidable)
+    return collidables_below
+
+
+def get_collidables_above(collidables, player):
+    collidables_above = []
+    for collidable in collidables:
+        if collidable.pos.y + collidable.collision_area_y <= player.pos.y + player.collision_area_y:
+            collidables_above.append(collidable)
+    return collidables_above
+
+
+def create_image_frame(tile_map_image, collidables_below, collidables_above, player, surface_width, surface_height, ):
+    image_frame = pygame.Surface((surface_width, surface_height))
+    image_frame.blit(tile_map_image, tile_map_image.get_rect())
+    for drawable in collidables_above:
+        image_frame.blit(drawable.image, drawable.pos)
+    image_frame.blit(player.image, player.pos)
+    for drawable in collidables_below:
+        image_frame.blit(drawable.image, drawable.pos)
+
+    return image_frame
+
+
 class Scene(object):
     def __init__(self, screen_width, screen_height, screen):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.screen = screen
 
-        self.map_layout = self.get_map_layout()
+        self.map_layout = get_map_layout()
         self.map_provider = MapProvider(32, self.map_layout)
         self.tile_map = self.map_provider.generate_map()
 
@@ -22,9 +70,9 @@ class Scene(object):
         tick_rate = 120
         screen_half_width = self.screen_width / 2
         screen_half_height = self.screen_height / 2
-        PLAYER_SPEED = 2
-        MAP_WIDTH = len(self.map_layout[0]) * 32
-        MAP_HEIGHT = len(self.map_layout) * 32
+        player_speed = 2
+        map_width = len(self.map_layout[0]) * 32
+        map_height = len(self.map_layout) * 32
         zoom_scale = 1
         tree = Structure(370, 400, "./objects/object_data/structures/tree.json")
         player = Sprite("./objects/object_data/sprites/player.json", self.screen_width / 2 - 24,
@@ -32,13 +80,13 @@ class Scene(object):
         npc = Sprite("./objects/object_data/sprites/player.json", 576, 384)
         stone_arch = Structure(256, 352, "./objects/object_data/structures/stone_arch.json")
 
-        objects = [npc, tree, stone_arch]
-        objects.extend(self.get_collidables())
+        collidables = [npc, tree, stone_arch]
+        collidables.extend(self.get_collidables())
         run = True
 
-        tilemap_surface = pygame.Surface((MAP_WIDTH, MAP_HEIGHT))
+        tile_map_surface = pygame.Surface((map_width, map_height))
         for drawable in self.tile_map:
-            tilemap_surface.blit(drawable.image, drawable.pos)
+            tile_map_surface.blit(drawable.image, drawable.pos)
 
         internal_surface_size = (2048, 1280)
         internal_surface = pygame.Surface(internal_surface_size, pygame.SRCALPHA)
@@ -48,10 +96,10 @@ class Scene(object):
         internal_offset.y = internal_surface_size[1] / 2 - screen_half_height
 
         while run:
-            objects_above = self.get_objects_above(objects, player)
-            objects_below = self.get_objects_below(objects, player)
-            image_frame_surface = self.create_image_frame(tilemap_surface, objects_below, objects_above, player,
-                                                          MAP_WIDTH, MAP_HEIGHT)
+            collidables_above = get_collidables_above(collidables, player)
+            collidables_below = get_collidables_below(collidables, player)
+            image_frame_surface = create_image_frame(tile_map_surface, collidables_below, collidables_above, player,
+                                                     map_width, map_height)
 
             camera_offset = pygame.math.Vector2(-player.pos.x + screen_half_width - 24,
                                                 -player.pos.y + screen_half_height - 24) + internal_offset
@@ -65,7 +113,7 @@ class Scene(object):
             player.animate(100)
             npc.animate(100)
 
-            self.check_controls(player, zoom_scale, objects, PLAYER_SPEED)
+            check_controls(player, zoom_scale, collidables, player_speed)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -83,46 +131,3 @@ class Scene(object):
             if collidable.collision:
                 collidables.append(collidable)
         return collidables
-
-    def get_map_layout(self):
-        with open('./resources/map1.json', 'r') as file:
-            return json.load(file)["map_layout"]
-
-    def get_objects_below(self, objects, player):
-        objects_below = []
-        for object in objects:
-            if object.pos.y + object.collision_area_y >= player.pos.y + player.collision_area_y:
-                objects_below.append(object)
-        return objects_below
-
-    def get_objects_above(self, objects, player):
-        objects_above = []
-        for object in objects:
-            if object.pos.y + object.collision_area_y <= player.pos.y + player.collision_area_y:
-                objects_above.append(object)
-        return objects_above
-
-    def create_image_frame(self, tilemap_image, objects_below, objects_above, player, surface_width, surface_height, ):
-        image_frame = pygame.Surface((surface_width, surface_height))
-        image_frame.blit(tilemap_image, tilemap_image.get_rect())
-        for drawable in objects_above:
-            image_frame.blit(drawable.image, drawable.pos)
-        image_frame.blit(player.image, player.pos)
-        for drawable in objects_below:
-            image_frame.blit(drawable.image, drawable.pos)
-
-        return image_frame
-
-    def check_controls(self, player, zoom_scale, objects, player_speed):
-        key = pygame.key.get_pressed()
-        if (key[pygame.K_LEFT] or key[pygame.K_a]) and not player.check_if_collides(objects, -player_speed, 0):
-            player.move_left(zoom_scale)
-        if (key[pygame.K_RIGHT] or key[pygame.K_d]) and not player.check_if_collides(objects, player_speed, 0):
-            player.move_right(zoom_scale)
-        if (key[pygame.K_DOWN] or key[pygame.K_s]) and not player.check_if_collides(objects, 0, player_speed):
-            player.move_down(zoom_scale)
-        if (key[pygame.K_UP] or key[pygame.K_w]) and not player.check_if_collides(objects, 0, -player_speed):
-            player.move_up(zoom_scale)
-        if not any((key[pygame.K_LEFT], key[pygame.K_RIGHT], key[pygame.K_DOWN], key[pygame.K_UP], key[pygame.K_w],
-                    key[pygame.K_a], key[pygame.K_s], key[pygame.K_d])):
-            player.state = "idle"
