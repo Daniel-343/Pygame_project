2ab9d6492f7f282fa62a4f5e4df90dc290b805a6
Daniel-343 <danthe34@gmail.com>
2024-02-15 17:09:44 +0100

    add SceneService

--- scenes/Scene.py
+++ scenes/Scene.py
@@ -6,68 +6,10 @@ import pygame
 from game_data import RouteProvider
 from map.MapProvider import MapProvider
 from objects.Structure import Structure
+from scenes import SceneService
 from scenes.TeleportBlock import TeleportBlock
 from sprites.Sprite import Sprite
 from utils import KeyListener
-
-
-
-def get_map_layout(map_layout_source):
-    with open(map_layout_source, 'r') as file:
-        return json.load(file)["map_layout"]
-
-
-def get_collidables_below(collidables, player):
-    collidables_below = []
-    for collidable in collidables:
-        if collidable.pos.y + collidable.collision_area_y >= player.pos.y + player.collision_area_y:
-            collidables_below.append(collidable)
-    return collidables_below
-
-
-def get_collidables_above(collidables, player):
-    collidables_above = []
-    for collidable in collidables:
-        if collidable.pos.y + collidable.collision_area_y < player.pos.y + player.collision_area_y:
-            collidables_above.append(collidable)
-    return collidables_above
-
-
-def create_image_frame(tile_map_image, collidables_below, collidables_above, player, surface_width, surface_height, ):
-    image_frame = pygame.Surface((surface_width, surface_height))
-    image_frame.blit(tile_map_image, tile_map_image.get_rect())
-    for drawable in collidables_above:
-        image_frame.blit(drawable.image, drawable.pos)
-    image_frame.blit(player.image, player.pos)
-    for drawable in collidables_below:
-        image_frame.blit(drawable.image, drawable.pos)
-
-    return image_frame
-
-
-def get_collidable_tiles(tile_map):
-    collidables = []
-    for collidable in tile_map:
-        if collidable.collision:
-            collidables.append(collidable)
-    return collidables
-
-
-def get_collidables(scene_data):
-    collidables = []
-    for collidable in scene_data["objects"]["structures"]:
-        collidables.append(
-            Structure(collidable["name"], collidable["initial_position_x"], collidable["initial_position_y"]))
-    for collidable in scene_data["objects"]["sprites"]:
-        collidables.append(
-            Sprite(collidable["name"], collidable["initial_position_x"], collidable["initial_position_y"]))
-    return collidables
-
-
-def get_scene_data(scene_name):
-    scene_route = RouteProvider.get_route_by_name(scene_name, "scene")
-    with open(scene_route, 'r') as file:
-        return json.load(file)["sceneData"]
 
 
 class Scene(object):
@@ -84,9 +26,9 @@ class Scene(object):
         self.screen_half_width = self.screen_width / 2
         self.screen_half_height = self.screen_height / 2
 
-        self.scene_data = get_scene_data(scene_name)
+        self.scene_data = SceneService.get_scene_data(scene_name)
 
-        self.map_layout = get_map_layout(RouteProvider.get_route_by_name(self.scene_data["map_name"], "map"))
+        self.map_layout = SceneService.get_map_layout(RouteProvider.get_route_by_name(self.scene_data["map_name"], "map"))
         self.map_provider = MapProvider(32, self.map_layout)
         self.tile_map = self.map_provider.generate_map()
 
@@ -102,8 +44,8 @@ class Scene(object):
 
         teleport_blocks = self.create_teleport_blocks()
 
-        collidables = get_collidables(self.scene_data)
-        collidables.extend(get_collidable_tiles(self.tile_map))
+        collidables = SceneService.get_collidables(self.scene_data)
+        collidables.extend(SceneService.get_collidable_tiles(self.tile_map))
 
         tile_map_surface = pygame.Surface((map_width, map_height))
         for drawable in self.tile_map:
@@ -117,9 +59,9 @@ class Scene(object):
         internal_offset.y = internal_surface_size[1] / 2 - self.screen_half_height
 
         while self.run:
-            collidables_above = get_collidables_above(collidables, player)
-            collidables_below = get_collidables_below(collidables, player)
-            image_frame_surface = create_image_frame(tile_map_surface, collidables_below, collidables_above, player,
+            collidables_above = SceneService.get_collidables_above(collidables, player)
+            collidables_below = SceneService.get_collidables_below(collidables, player)
+            image_frame_surface = SceneService.create_image_frame(tile_map_surface, collidables_below, collidables_above, player,
                                                      map_width, map_height)
 
             self.show_screen_surface(image_frame_surface, player, internal_surface,
---
+++ scenes/SceneService.py
@@ -0,0 +1,65 @@
+import json
+
+import pygame
+
+from game_data import RouteProvider
+from objects.Structure import Structure
+from sprites.Sprite import Sprite
+
+
+def get_map_layout(map_layout_source):
+    with open(map_layout_source, 'r') as file:
+        return json.load(file)["map_layout"]
+
+
+def get_collidables_below(collidables, player):
+    collidables_below = []
+    for collidable in collidables:
+        if collidable.pos.y + collidable.collision_area_y >= player.pos.y + player.collision_area_y:
+            collidables_below.append(collidable)
+    return collidables_below
+
+
+def get_collidables_above(collidables, player):
+    collidables_above = []
+    for collidable in collidables:
+        if collidable.pos.y + collidable.collision_area_y < player.pos.y + player.collision_area_y:
+            collidables_above.append(collidable)
+    return collidables_above
+
+
+def create_image_frame(tile_map_image, collidables_below, collidables_above, player, surface_width, surface_height, ):
+    image_frame = pygame.Surface((surface_width, surface_height))
+    image_frame.blit(tile_map_image, tile_map_image.get_rect())
+    for drawable in collidables_above:
+        image_frame.blit(drawable.image, drawable.pos)
+    image_frame.blit(player.image, player.pos)
+    for drawable in collidables_below:
+        image_frame.blit(drawable.image, drawable.pos)
+
+    return image_frame
+
+
+def get_collidable_tiles(tile_map):
+    collidables = []
+    for collidable in tile_map:
+        if collidable.collision:
+            collidables.append(collidable)
+    return collidables
+
+
+def get_collidables(scene_data):
+    collidables = []
+    for collidable in scene_data["objects"]["structures"]:
+        collidables.append(
+            Structure(collidable["name"], collidable["initial_position_x"], collidable["initial_position_y"]))
+    for collidable in scene_data["objects"]["sprites"]:
+        collidables.append(
+            Sprite(collidable["name"], collidable["initial_position_x"], collidable["initial_position_y"]))
+    return collidables
+
+
+def get_scene_data(scene_name):
+    scene_route = RouteProvider.get_route_by_name(scene_name, "scene")
+    with open(scene_route, 'r') as file:
+        return json.load(file)["sceneData"]
